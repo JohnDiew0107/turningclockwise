9e908dc07f02f9dd8999686a62407909b7b47684
John <John@192.168.1.70>
2014-03-05 22:47:07 +0800

    Initial version

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.turningclockwise.com/admin/');
+define('HTTP_CATALOG', 'http://www.turningclockwise.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.turningclockwise.com/admin/');
+define('HTTPS_CATALOG', 'http://www.turningclockwise.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/content/99/8184899/html/turningclockwise/admin/');
+define('DIR_SYSTEM', '/home/content/99/8184899/html/turningclockwise/system/');
+define('DIR_DATABASE', '/home/content/99/8184899/html/turningclockwise/system/database/');
+define('DIR_LANGUAGE', '/home/content/99/8184899/html/turningclockwise/admin/language/');
+define('DIR_TEMPLATE', '/home/content/99/8184899/html/turningclockwise/admin/view/template/');
+define('DIR_CONFIG', '/home/content/99/8184899/html/turningclockwise/system/config/');
+define('DIR_IMAGE', '/home/content/99/8184899/html/turningclockwise/image/');
+define('DIR_CACHE', '/home/content/99/8184899/html/turningclockwise/system/cache/');
+define('DIR_DOWNLOAD', '/home/content/99/8184899/html/turningclockwise/download/');
+define('DIR_LOGS', '/home/content/99/8184899/html/turningclockwise/system/logs/');
+define('DIR_CATALOG', '/home/content/99/8184899/html/turningclockwise/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'turningclockwise.db.8184899.hostedresource.com');
+define('DB_USERNAME', 'turningclockwise');
+define('DB_PASSWORD', 'DadaCaTraQ74!');
+define('DB_DATABASE', 'turningclockwise');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.turningclockwise.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.turningclockwise.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/content/99/8184899/html/turningclockwise/catalog/');
+define('DIR_SYSTEM', '/home/content/99/8184899/html/turningclockwise/system/');
+define('DIR_DATABASE', '/home/content/99/8184899/html/turningclockwise/system/database/');
+define('DIR_LANGUAGE', '/home/content/99/8184899/html/turningclockwise/catalog/language/');
+define('DIR_TEMPLATE', '/home/content/99/8184899/html/turningclockwise/catalog/view/theme/');
+define('DIR_CONFIG', '/home/content/99/8184899/html/turningclockwise/system/config/');
+define('DIR_IMAGE', '/home/content/99/8184899/html/turningclockwise/image/');
+define('DIR_CACHE', '/home/content/99/8184899/html/turningclockwise/system/cache/');
+define('DIR_DOWNLOAD', '/home/content/99/8184899/html/turningclockwise/download/');
+define('DIR_LOGS', '/home/content/99/8184899/html/turningclockwise/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'turningclockwise.db.8184899.hostedresource.com');
+define('DB_USERNAME', 'turningclockwise');
+define('DB_PASSWORD', 'DadaCaTraQ74!');
+define('DB_DATABASE', 'turningclockwise');
+define('DB_PREFIX', 'oc_');
+?>
